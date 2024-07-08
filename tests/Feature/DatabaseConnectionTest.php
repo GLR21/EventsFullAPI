16ce7743a960daa1b72c6b45bb296ddd0412e81a
Gabriel Lange Ramos <gabriellange.ramos@gmail.com>
2024-07-08 20:47:31 -0300

    feat: Adding user tests

---
+++ tests/Feature/DatabaseConnectionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class DatabaseConnectionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testDatabaseConnection()
+    {
+        try
+        {
+            DB::connection()->getPdo();
+            $this->assertTrue(true, 'Conexão estabelecida com a database.');
+        } catch (\Exception $e)
+        {
+            $this->fail('Não foi possível estabelecer conexão com a database: ' . $e->getMessage());
+        }
+    }
+}
