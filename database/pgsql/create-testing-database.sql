53b953f85e19e25389ecbb86579d05718eea395f
Gabriel Lange Ramos <gabriellange.ramos@gmail.com>
2024-07-16 14:40:39 -0300

    adding create database testing

---
+++ database/pgsql/create-testing-database.sql
@@ -0,0 +1,18 @@
+-- Create the testing database if it doesn't exist
+SELECT 'CREATE DATABASE testing'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testing')\gexec
+
+-- Create the testing role as a superuser if it doesn't exist
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE rolname = 'testing') THEN
+      CREATE ROLE testing WITH LOGIN SUPERUSER PASSWORD 'testing';
+   END IF;
+END
+$do$;
+
+-- Grant all privileges on the testing database to the testing role
+GRANT ALL PRIVILEGES ON DATABASE testing TO testing;
